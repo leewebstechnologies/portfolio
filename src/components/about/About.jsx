eb0bf4fc3bb2d557b40e3b32366528a37d751152
leewebstechnologies <aliuajibade317@gmail.com>
2022-11-10 21:48:39 -0800

    about page completd

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,4 +1,5 @@
 import "./about.css";
+import Award from "../../images/award.png";
 
 const About = () => {
   return (
@@ -19,10 +20,24 @@ const About = () => {
           It is a long established fact that a reader will be distracted by the
           readable content.
         </p>
-        <p className="a-desc"> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+        <p className="a-desc">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
           eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
           minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-          aliquip ex ea commodo consequat duis aute irure dolor in reprehende.</p>
+          aliquip ex ea commodo consequat duis aute irure dolor in reprehende.
+        </p>
+        <div className="a-award">
+          <img src={Award} alt="" className="a-award-img" />
+          <div className="a-award-texts">
+            <h4 className="a-award-title">International Design Awards</h4>
+            <p className="a-award-desc">
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nam
+              aspernatur deserunt soluta non! Nostrum minus eos neque placeat
+              omnis rem delectus quidem voluptate saepe, qui in nihil, sunt quod
+              praesentium.
+            </p>
+          </div>
+        </div>
       </div>
     </div>
   );
