444340c717d483b7f4a618ee4b2279d70d86ea77
leewebstechnologies <aliuajibade317@gmail.com>
2022-11-12 07:36:57 -0800

    data.js

--- src/components/productList/ProductList.jsx
+++ src/components/productList/ProductList.jsx
@@ -1,25 +1,22 @@
 import Product from "../product/Product";
 import "./productList.css";
+import { products } from "../../data";
 
 const ProductList = () => {
   return (
     <div className="pl">
       <div className="pl-texts">
         <h1 className="pl-title">Create & Inspire. It's Leewebs</h1>
-        <p className="pl-desc">Leewebs is a creative portfolio that your work has been waiting for.
+        <p className="pl-desc">
+          Leewebs is a creative portfolio that your work has been waiting for.
           Beautiful homes, stunning portfolio styles & a whole lot more awaits
-          inside.</p>
+          inside.
+        </p>
       </div>
       <div className="pl-list">
-        <Product />
-        <Product />
-        <Product />
-        <Product />
-        <Product />
-        <Product />
-        <Product />
-        <Product />
-     
+        {products.map((item) => (
+          <Product />
+        ))}
       </div>
     </div>
   );
