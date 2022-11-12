5da91e445e476bd3cd917449a4d0a15cbdbdd3f1
leewebstechnologies <aliuajibade317@gmail.com>
2022-11-11 21:28:26 -0800

    product page added

--- src/components/productList/ProductList.jsx
+++ src/components/productList/ProductList.jsx
@@ -1,7 +1,28 @@
+import Product from "../product/Product";
 import "./productList.css";
 
 const ProductList = () => {
-  return <div>ProductList</div>;
+  return (
+    <div className="pl">
+      <div className="pl-texts">
+        <h1 className="pl-title">Create & Inspire. It's Leewebs</h1>
+        <p className="pl-desc">Leewebs is a creative portfolio that your work has been waiting for.
+          Beautiful homes, stunning portfolio styles & a whole lot more awaits
+          inside.</p>
+      </div>
+      <div className="pl-list">
+        <Product />
+        <Product />
+        <Product />
+        <Product />
+        <Product />
+        <Product />
+        <Product />
+        <Product />
+     
+      </div>
+    </div>
+  );
 };
 
 export default ProductList;
