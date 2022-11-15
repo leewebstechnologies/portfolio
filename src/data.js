1082f4c13646283c1b6a3508f5c06d73e27565ae
leewebstechnologies <aliuajibade317@gmail.com>
2022-11-14 22:18:50 -0800

    commit

--- src/data.js
+++ src/data.js
@@ -6,27 +6,27 @@ export const products = [
   },
   {
     id: 2,
-    img: "https://buildfire.com/wp-content/uploads/2021/10/Whats-the-Difference-Between-UX-Design-vs.-UI-Design_@3x-1-scaled.jpg",
+    img: "https://img.freepik.com/free-vector/flat-design-ui-ux-landing-page_52683-72187.jpg?w=1380&t=st=1668458548~exp=1668459148~hmac=6bded93a46d7f69e57934ce6267041454c50ff616aed5d99d8e04d1bbd3a5717",
     link: "http://lama.dev",
   },
   {
     id: 3,
-    img: "https://www.figma.com/file/CPQL1piDSTyAd06gfUTKag/FoodMania-App?node-id=0%3A1&t=GFS3X76OEmcia8Us-0",
+    img: "https://adanitcenter.com/wp-content/uploads/2022/09/ui-ux-design-company-in-bangalore.png",
     link: "http://lama.dev",
   },
   {
     id: 4,
-    img: "https://i.ibb.co/x6Jgzj5/4.jpg",
+    img: "https://img.freepik.com/free-vector/realistic-ui-ux-landing-page-template_52683-68898.jpg?w=1380&t=st=1668458236~exp=1668458836~hmac=8128eb920fa674ce9adab12660ef088466aa53f0806f119e8d607c36ba631123",
     link: "http://lama.dev",
   },
   {
     id: 5,
-    img: "https://i.ibb.co/k2j4Zpq/5.jpg",
+    img: "https://img.freepik.com/free-vector/restaurant-ui-app-template_47987-1881.jpg?w=1380&t=st=1668458361~exp=1668458961~hmac=95b62d1c3de13cb7eb709ba0fd75bc3c0e722529b9868871bc98fe2ad8d135c6",
     link: "http://lama.dev",
   },
   {
     id: 6,
-    img: "https://i.ibb.co/rw1F4NH/6.jpg",
+    img: "https://img.freepik.com/free-vector/realistic-ui-ux-background_52683-69487.jpg?w=1380&t=st=1668458415~exp=1668459015~hmac=fc94bf3b06df886bff56962f1a21bffc3b1adae46a77ddcbfe83c884bf09617d",
     link: "http://lama.dev",
   },
 ];
