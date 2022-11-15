8f513f59228c385e27ab1472c5084559a132f83f
leewebstechnologies <aliuajibade317@gmail.com>
2022-11-14 21:35:36 -0800

    fifth commit

--- src/components/productList/ProductList.jsx
+++ src/components/productList/ProductList.jsx
@@ -15,7 +15,7 @@ const ProductList = () => {
       </div>
       <div className="pl-list">
         {products.map((item) => (
-          <Product />
+          <Product key={item.id} img={item.img} link={item.link} />
         ))}
       </div>
     </div>
--- src/data.js
+++ src/data.js
@@ -1,32 +1,32 @@
 export const products = [
   {
     id: 1,
-    img: "https://i.ibb.co/4TD6cYF/1.jpg",
+    img: "https://userguiding.com/wp-content/uploads/2021/02/ux-ui-trends-neuromorphic-2.png",
     link: "http://lama.dev",
   },
   {
     id: 2,
-    img: "https://i.ibb.co/4TD6cYF/1.jpg",
+    img: "https://buildfire.com/wp-content/uploads/2021/10/Whats-the-Difference-Between-UX-Design-vs.-UI-Design_@3x-1-scaled.jpg",
     link: "http://lama.dev",
   },
   {
     id: 3,
-    img: "https://i.ibb.co/4TD6cYF/1.jpg",
+    img: "https://www.figma.com/file/CPQL1piDSTyAd06gfUTKag/FoodMania-App?node-id=0%3A1&t=GFS3X76OEmcia8Us-0",
     link: "http://lama.dev",
   },
   {
     id: 4,
-    img: "https://i.ibb.co/4TD6cYF/1.jpg",
+    img: "https://i.ibb.co/x6Jgzj5/4.jpg",
     link: "http://lama.dev",
   },
   {
     id: 5,
-    img: "https://i.ibb.co/4TD6cYF/1.jpg",
+    img: "https://i.ibb.co/k2j4Zpq/5.jpg",
     link: "http://lama.dev",
   },
   {
     id: 6,
-    img: "https://i.ibb.co/4TD6cYF/1.jpg",
+    img: "https://i.ibb.co/rw1F4NH/6.jpg",
     link: "http://lama.dev",
   },
 ];
