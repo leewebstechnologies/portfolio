9db60e3e79e9929ff0a9290277f091684667bf9f
leewebstechnologies <aliuajibade317@gmail.com>
2022-11-17 07:17:05 -0800

    added

--- src/data.js
+++ src/data.js
@@ -2,31 +2,31 @@ export const products = [
   {
     id: 1,
     img: "https://userguiding.com/wp-content/uploads/2021/02/ux-ui-trends-neuromorphic-2.png",
-    link: "http://lama.dev",
+    link: "https://github.com/leewebstechnologies",
   },
   {
     id: 2,
     img: "https://img.freepik.com/free-vector/flat-design-ui-ux-landing-page_52683-72187.jpg?w=1380&t=st=1668458548~exp=1668459148~hmac=6bded93a46d7f69e57934ce6267041454c50ff616aed5d99d8e04d1bbd3a5717",
-    link: "http://lama.dev",
+    link: "https://github.com/leewebstechnologies",
   },
   {
     id: 3,
     img: "https://adanitcenter.com/wp-content/uploads/2022/09/ui-ux-design-company-in-bangalore.png",
-    link: "http://lama.dev",
+    link: "https://github.com/leewebstechnologies",
   },
   {
     id: 4,
     img: "https://img.freepik.com/free-vector/realistic-ui-ux-landing-page-template_52683-68898.jpg?w=1380&t=st=1668458236~exp=1668458836~hmac=8128eb920fa674ce9adab12660ef088466aa53f0806f119e8d607c36ba631123",
-    link: "http://lama.dev",
+    link: "https://github.com/leewebstechnologies",
   },
   {
     id: 5,
     img: "https://img.freepik.com/free-vector/restaurant-ui-app-template_47987-1881.jpg?w=1380&t=st=1668458361~exp=1668458961~hmac=95b62d1c3de13cb7eb709ba0fd75bc3c0e722529b9868871bc98fe2ad8d135c6",
-    link: "http://lama.dev",
+    link: "https://github.com/leewebstechnologies",
   },
   {
     id: 6,
     img: "https://dpbnri2zg3lc2.cloudfront.net/en/wp-content/uploads/old-blog-uploads/tasks-and-responsibilities-of-ux-vs-ui-designers.jpg",
-    link: "http://lama.dev",
+    link: "https://github.com/leewebstechnologies",
   },
 ];
