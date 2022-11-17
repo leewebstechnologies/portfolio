8d6a79e327e3f99ced81c5f676e04e7e5532e307
leewebstechnologies <aliuajibade317@gmail.com>
2022-11-16 17:05:23 -0800

    project completed

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -14,24 +14,30 @@ const About = () => {
       <div className="a-right">
         <h1 className="a-title">About Me</h1>
         <p className="a-subject">
-          It is a long established fact that a reader will be distracted by the
-          readable content.
+          I attended Zion African Church Primary school Alagbaa, Iyana-Ipaja,
+          Agege from 1991 to 1996 and State high school Oyewole, Agege from 1996
+          to 2002 for my primary and secondary education respectively. I
+          graduated in Accounting from the university of Lagos in 2014. I attend
+          Annahdah institute of Arabic and Islamic studies from 2007 till date.
+          I attended Zad Academy, Makkah, Kingdom of Saudi Arabia where I bagged
+          a diploma in Islamic Law (with a distinction) from 2020 to 2022. I
+          worked as a software developer and graphic designer intern in Techend
+          Incubation Limited, Oshodi, Lagos from 2018 to 2020. I now work as a
+          freelancer - Graphic designer and web developer.
         </p>
         <p className="a-desc">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          {/* Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
           eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
           minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-          aliquip ex ea commodo consequat duis aute irure dolor in reprehende.
+          aliquip ex ea commodo consequat duis aute irure dolor in reprehende. */}
         </p>
         <div className="a-award">
           <img src={Award} alt="" className="a-award-img" />
           <div className="a-award-texts">
             <h4 className="a-award-title">International Design Awards</h4>
             <p className="a-award-desc">
-              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nam
-              aspernatur deserunt soluta non! Nostrum minus eos neque placeat
-              omnis rem delectus quidem voluptate saepe, qui in nihil, sunt quod
-              praesentium.
+              We can help you build better websites using React Js and its
+              ecosystem.
             </p>
           </div>
         </div>
